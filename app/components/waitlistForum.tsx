62233d07e58be8ce7c1ddf7520fa03d3a4f14de6
JunaidPeer <mdjunaidpeer@gmail.com>
2023-11-24 12:39:45 +0000

    firebase

--- app/components/waitlistForum.tsx
+++ app/components/waitlistForum.tsx
@@ -1,27 +1,28 @@
 import React, { useState } from 'react';
-// import firebase from 'firebase';
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import {db} from '../lib/firebase';
 
 const WaitlistForum = () => {
   const [email, setEmail] = useState('');
   const [walletAddress, setWalletAddress] = useState('');
   const [isShared, setIsShared] = useState(false);
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+    const handleSubmit = async (e: React.FormEvent) => {
+      e.preventDefault();
 
-    try {
-      // Update data to Firebase
-      await firebase.firestore().collection('waitlist').add({
-        email,
-        walletAddress,
-      });
+      try {
+        // Update data to Firebase
+        await setDoc(doc(db, 'waitlist'), {
+          email,
+          walletAddress,
+        });
 
-      // Set isShared to true
-      setIsShared(true);
-    } catch (error) {
-      console.error('Error updating data to Firebase:', error);
-    }
-  };
+        // Set isShared to true
+        setIsShared(true);
+      } catch (error) {
+        console.error('Error updating data to Firebase:', error);
+      }
+    };
 
   const handleTweet = () => {
     // Handle tweet functionality here
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import styles from './page.module.css'
 import React from 'react';
 import NavBar from './components/navBar'
 import BackDrop from './images/mobile.png'
+import WaitlistForum from './components/waitlistForum'
+
 export default function Home() {
   return (
     
@@ -11,7 +13,9 @@ export default function Home() {
     <div className={styles.overlap}></div>
   <h1 className={styles.h1}> SplitWave </h1>
   <p className={styles.para}> SplitWave streamlines expense sharing by effortlessly dividing bills among friends, offering a hassle-free solution for managing group finances. Simplify your financial interactions with this user-friendly app.</p>
-  <button className={styles.button}> Join Waitlist </button>
+  <button className={styles.button}
+  onClick={WaitlistForum}
+  > Join Waitlist </button>
   {/* <Image
     src={BackDrop}
     alt="backdrop"
