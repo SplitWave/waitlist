cd7da888b7f1f0cf1f7a062461d24b07bd5b935b
JunaidPeer <mdjunaidpeer@gmail.com>
2023-11-27 17:10:11 +0000

    imgURL

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
-
+const imgURL = "https://file.notion.so/f/f/d73cd775-91a8-4371-86e0-f8e43167c197/e63bffc9-460f-4fdc-b303-2274a6bea67f/metadata.png?id=e365abe4-5c36-4d0a-a8fe-ec71aef3ab92&table=block&spaceId=d73cd775-91a8-4371-86e0-f8e43167c197&expirationTimestamp=1701216000000&signature=Fp9OYa9dyhMTCGPFUH2gNn-IrBN2teh417SFv2_R7ow&downloadName=metadata.png";
 
 const rubik = Rubik({ subsets: ["latin"] });
 
@@ -10,9 +10,20 @@ export const metadata: Metadata = {
   title: "SplitWave",
   description:
     "Ditch the finance chaos! SplitWave on Solana brings fun to your money — seamless payments, bill splits, and expense tracking. Join the waitlist for on-chain finance, but cooler! 🚀💸",
-    openGraph: {
-      images: './metadata.png',
-    },
+openGraph: {
+    title: "SplitWave",
+    description:
+      "Ditch the finance chaos! SplitWave on Solana brings fun to your money — seamless payments, bill splits, and expense tracking. Join the waitlist for on-chain finance, but cooler! 🚀💸",
+    images: [
+      {
+        url: imgURL,
+        width: 800,
+        height: 600,
+        alt: "SplitWave",
+      },
+    ],
+    
+  },
   };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import background from "../public/images/background.png";
 import Head from "next/head";
 
 export default function Home() {
+  const imgURL = "https://file.notion.so/f/f/d73cd775-91a8-4371-86e0-f8e43167c197/e63bffc9-460f-4fdc-b303-2274a6bea67f/metadata.png?id=e365abe4-5c36-4d0a-a8fe-ec71aef3ab92&table=block&spaceId=d73cd775-91a8-4371-86e0-f8e43167c197&expirationTimestamp=1701216000000&signature=Fp9OYa9dyhMTCGPFUH2gNn-IrBN2teh417SFv2_R7ow&downloadName=metadata.png";
   const [isModalOpen, setModalOpen] = useState<boolean>(false);
 
   const openModal = () => {
@@ -26,7 +27,8 @@ export default function Home() {
         property="og:description"
         content="Ditch the finance chaos! SplitWave on Solana brings fun to your money — seamless payments, bill splits, and expense tracking. Join the waitlist for on-chain finance, but cooler! 🚀💸"
       />
-      <meta property="og:image" content="./metadata.png" />
+      <meta property="og:image" content={imgURL} />
+      <meta name="twitter:image" content={imgURL} />
     </Head>
     <div className="w-full s:h-[56.25rem] md:h-[68.75rem] relative  text-white">
       <NavBar />
