9119721468cf0c890936cf1716e54fdb90dedb07
JunaidPeer <mdjunaidpeer@gmail.com>
2023-11-27 15:07:27 +0000

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 const rubik = Rubik({ subsets: ["latin"] });
 
@@ -8,7 +9,10 @@ export const metadata: Metadata = {
   title: "SplitWave",
   description:
     "Ditch the finance chaos! SplitWave on Solana brings fun to your money — seamless payments, bill splits, and expense tracking. Join the waitlist for on-chain finance, but cooler! 🚀💸",
-};
+    openGraph: {
+      images: '../public/images/metadata.png',
+    },
+  };
 
 export default function RootLayout({
   children,
@@ -19,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={rubik.className}>
         <main className=" bg-[#0E1210]  ">{children}</main>
+        <Analytics />
       </body>
     </html>
   );
