8c559060d1fa1e632f69f8a37188b0782a338ea5
JunaidPeer <mdjunaidpeer@gmail.com>
2023-11-27 17:14:42 +0000

    og image update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
-const imgURL = "https://file.notion.so/f/f/d73cd775-91a8-4371-86e0-f8e43167c197/e63bffc9-460f-4fdc-b303-2274a6bea67f/metadata.png?id=e365abe4-5c36-4d0a-a8fe-ec71aef3ab92&table=block&spaceId=d73cd775-91a8-4371-86e0-f8e43167c197&expirationTimestamp=1701216000000&signature=Fp9OYa9dyhMTCGPFUH2gNn-IrBN2teh417SFv2_R7ow&downloadName=metadata.png";
+const imgURL = "https://github.com/SplitWave/waitlist/blob/main/app/metadata.png?raw=true";
 
 const rubik = Rubik({ subsets: ["latin"] });
 
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import background from "../public/images/background.png";
 import Head from "next/head";
 
 export default function Home() {
-  const imgURL = "https://file.notion.so/f/f/d73cd775-91a8-4371-86e0-f8e43167c197/e63bffc9-460f-4fdc-b303-2274a6bea67f/metadata.png?id=e365abe4-5c36-4d0a-a8fe-ec71aef3ab92&table=block&spaceId=d73cd775-91a8-4371-86e0-f8e43167c197&expirationTimestamp=1701216000000&signature=Fp9OYa9dyhMTCGPFUH2gNn-IrBN2teh417SFv2_R7ow&downloadName=metadata.png";
+  const imgURL = "https://github.com/SplitWave/waitlist/blob/main/app/metadata.png?raw=true";
   const [isModalOpen, setModalOpen] = useState<boolean>(false);
 
   const openModal = () => {
