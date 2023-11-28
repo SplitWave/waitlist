7f662a7140d947ac35731b49f5e895dbe122ec2b
timadigwe <timothyadigwe2019@gmail.com>
2023-11-28 16:05:52 +0100

    ui fixes

--- app/page.tsx
+++ app/page.tsx
@@ -33,10 +33,10 @@ export default function Home() {
       </Head>
       <div className="w-full md:h-[75rem] s:h-[56.25rem] bg-[#0E1210]  text-white">
         <NavBar />
-        <h1 className=" bg-clip-text font-semibold s:text-[3.75rem] md:text-[8rem] text-center mt-[3.75rem] text-transparent bg-gradient-to-t from-[#FFFFFF]  to-[#6B9984] ">
+        <h1 className=" bg-clip-text font-semibold s:text-[2.8125rem] md:text-[8rem] text-center mt-[3.75rem] text-transparent bg-gradient-to-t from-[#FFFFFF]  to-[#6B9984] ">
           SplitWave
         </h1>
-        <p className=" lg:w-[66.5rem] md:w-full s:w-full m-auto mt-[1.25rem] s:px-[0.625rem] md:px-[0rem] s:text-[1.125rem] md:text-[1.5rem] font-normal text-center leading-[1.75rem] ">
+        <p className=" lg:w-[66.5rem] md:w-full s:w-full m-auto mt-[1.25rem] s:px-[0.625rem] md:px-[0rem] s:text-[1rem] md:text-[1.5rem] font-normal text-center leading-[1.75rem] ">
           Your go-to Solana app for all things money! From seamless bill
           splitting and payments to tracking expenses and analyzing spending
           habits. Connect socially, share financial wisdom, and explore a world
@@ -45,7 +45,7 @@ export default function Home() {
           meets fun! 🌊💰
         </p>
         <div
-          className=" s:mt-[1.5625rem] md:mt-[60px] relative z-10 m-auto flex bg-[#6B9984]  text-white s:text-[1.125rem] md:text-[1.625rem] font-normal s:w-[9.375rem] md:w-[13.5rem] s:h-[2.5rem] md:h-[3.9375rem] items-center justify-center rounded-[1.125rem]  "
+          className=" s:mt-[1.5625rem] md:mt-[60px] relative z-10 m-auto flex bg-[#6B9984]  text-white s:text-[1rem] md:text-[1.625rem] font-normal s:w-[9.375rem] md:w-[13.5rem] s:h-[2.5rem] md:h-[3.9375rem] items-center justify-center rounded-[1.125rem]  "
           onClick={openModal}
         >
           <h1>Join Waitlist</h1>
